aef7c6ed23d032bb7592f769f99497d2d86c95f9
Hendrik Louis Mahdi <hendrikmahdi@gmail.com>
2024-04-14 23:22:39 +0700

    Fix real-time character and word count, and case conversion functions
    - Wrapped all JavaScript code in a DOMContentLoaded event listener to ensure that the script is executed after the entire document has been loaded.
    - Made the case conversion functions globally accessible by attaching them to the window object.

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
-function upperCase() {
-  text = document.getElementById("input_text").value;
-  document.getElementById("input_text").value = text.toUpperCase();
-}
+document.addEventListener("DOMContentLoaded", function () {
+  window.upperCase = function () {
+    let text = document.getElementById("input_text").value;
+    document.getElementById("input_text").value = text.toUpperCase();
+  };
 
-function lowerCase() {
-  text = document.getElementById("input_text").value;
-  document.getElementById("input_text").value = text.toLowerCase();
-}
+  window.lowerCase = function () {
+    let text = document.getElementById("input_text").value;
+    document.getElementById("input_text").value = text.toLowerCase();
+  };
 
-function capitalCase() {
-  let text = document.getElementById("input_text").value;
-  let final_text = text
-    .toLowerCase()
-    .replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase());
-  document.getElementById("input_text").value = final_text;
-}
+  window.capitalCase = function () {
+    let text = document.getElementById("input_text").value;
+    let final_text = text
+      .toLowerCase()
+      .replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase());
+    document.getElementById("input_text").value = final_text;
+  };
 
-// Character counter
+  let text = document.getElementById("input_text");
+  text.addEventListener("input", () => {
+    let counter = text.value.split(/\s+/).filter(function (word) {
+      return word.length > 0;
+    }).length;
+    document.getElementById(
+      "word_count"
+    ).textContent = `Word count : ${counter}`;
+  });
 
-let text = document.getElementById("input_text");
-text.addEventListener("input", () => {
-  let counter = text.value.length;
-  document.getElementById(
-    "char_count"
-  ).textContent = `Character count : ${counter}`;
-});
-
-// Word counter
-text.addEventListener("input", () => {
-  let counter = text.value.split(/\s+/).filter(function (word) {
-    return word.length > 0;
-  }).length;
-  document.getElementById("word_count").textContent = `Word count : ${counter}`;
+  text.addEventListener("input", () => {
+    let counter = text.value.length;
+    document.getElementById(
+      "char_count"
+    ).textContent = `Character count : ${counter}`;
+  });
 });
